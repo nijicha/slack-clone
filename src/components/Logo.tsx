514dc017ff0a367f35ac65878fd863b91fe1724d
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2021-08-07 00:09:03 +0700

    Re-align folder structure

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { chakra, keyframes, ImageProps, forwardRef, usePrefersReducedMotion } from '@chakra-ui/react'
-import logo from './logo.svg'
+import { chakra, forwardRef, ImageProps, keyframes, usePrefersReducedMotion } from '@chakra-ui/react'
+import logo from '../assets/images/logo.svg'
 
 const spin = keyframes`
   from { transform: rotate(0deg); }
