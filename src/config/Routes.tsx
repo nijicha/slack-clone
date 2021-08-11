d1c0bdabe43207014ce05dce9cf3d75cb32cd880
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2021-08-12 03:01:45 +0700

    Create an examples components

--- src/config/Routes.tsx
+++ src/config/Routes.tsx
@@ -4,12 +4,18 @@ import { BrowserRouter as Switch, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Login from '../pages/Auth/Login'
 import Register from '../pages/Auth/Register'
+import Blogs from '../pages/Blogs'
+
+import TryReactHooks from '../components/TryReactHooks'
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/login" component={Login} />
     <Route path="/register" component={Register} />
+    <Route path="/blogs" component={Blogs} />
+
+    <Route path="/try-react-hooks" component={TryReactHooks} />
   </Switch>
 )
 
